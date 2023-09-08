3c6af82d81f78057ca0bc430db2fd854b0f1f429
nashdev255 <yutori1107@gmail.com>
2023-09-08 16:04:16 +0900

    add function.ts

---
+++ tsFunctions/function.ts
@@ -0,0 +1,10 @@
+/* popular */
+
+/* declare */
+function fucntionName(x: number, y: number) : void {
+    console.log(x + y);
+}
+// function functionName(x = 0, y = 0) to set default value
+
+/* execute */
+fucntionName(2, 3);
