708c9ff7433feaf5bec1729451c45126390f9582
nashdev255 <yutori1107@gmail.com>
2023-09-08 16:24:55 +0900

    add function4.ts

---
+++ tsFunctions/function4.ts
@@ -0,0 +1,9 @@
+/* avoid using function keyword ( arrow operator ) */
+
+/* declare */
+var value2Name = (argName: string) : void => {
+    console.log(argName);
+}
+
+/* execute */
+value2Name("Welcome to my Portfolio");
