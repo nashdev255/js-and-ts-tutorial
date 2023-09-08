791ed6d0f8279dafd971f1fedeeea4752cd5b2f7
nashdev255 <yutori1107@gmail.com>
2023-09-08 16:05:53 +0900

    add function2.ts

---
+++ tsFunctions/function2.ts
@@ -0,0 +1,10 @@
+/* noname function */
+
+/* declare */
+var valueName = function(x: string, y: string) {
+    return x + y;
+}
+
+/* execute */
+var value = valueName("Hello, ", "World");
+console.log(value);
