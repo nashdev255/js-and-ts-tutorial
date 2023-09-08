a35f91627918771ce4520266a2476acc3c9f8108
nashdev255 <yutori1107@gmail.com>
2023-09-08 15:18:43 +0900

    init

---
+++ function.js
@@ -0,0 +1,9 @@
+/* Popular */
+
+/* declare */
+function functionName() {
+    console.log("Hello, World");
+}
+
+/* execute */
+functionName();
---
+++ function2.js
@@ -0,0 +1,6 @@
+/* function literal */
+
+/* declare */
+var valueName = functionName() {
+    
+}
