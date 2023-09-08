24f8ab1f6a15e36debcd58022a6fe93ecbb54365
nashdev255 <yutori1107@gmail.com>
2023-09-08 15:49:36 +0900

    add function4

---
+++ function4.js
@@ -0,0 +1,9 @@
+/* avoid using function keyword ( arrow operator ) */
+
+/* declare */
+var valueName = (argName) => {
+    console.log(argName);
+}
+
+/* execute */
+valueName("Welcome to my Portfolio");
